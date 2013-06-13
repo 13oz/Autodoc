19f219335df619e9a9b1f2cf65a36a557bf6273d
13oz <duminsky.nick@gmail.com>
2013-06-13 17:00:24 +0400

    autodoc
    reorganized import section
    
    deleted comments

--- autodoc.py
+++ autodoc.py
@@ -1,18 +1,15 @@
-import argparse
 import os
+import argparse
+
 import reader
 import walker
 
 parser = argparse.ArgumentParser(description='foo')
 
-"""parser.add_argument('-f', '--filename', help='input filename', required=False)
-parser.add_argument('-d', '--dirname', help='input dir name', required=False)"""
 parser.add_argument('-t', '--target', help='target file or directory', required=False)
 parser.add_argument('-l', '--log', help='log file name', required=False)
 
 args = parser.parse_args()
-
-#print(args, file=args.log)
 
 if os.path.isfile(args.target):
 	reader.readFile(args.target.replace("\\", "/"), open(args.log, 'w'))
