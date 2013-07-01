35901b829d6ec68e809d1916bf363d023f6e3b69
13oz <duminsky.nick@gmail.com>
2013-07-01 17:20:16 +0400

    created reader-xml file

--- walker.py
+++ walker.py
@@ -7,6 +7,7 @@ def walkThrough(folder, errFile=sy.stdout):
 	for dir in [dir for dir in os.listdir(folder) if os.path.isdir(dir)]:
 		for file in [file for file in os.listdir(folder) if (os.path.isfile(file) and checkFile(file))]:
 			reader.readFile(file)
+		for elem in [elem for elem in os.listdir(dir) if os.path.isdir(elem)]: walkThrough(elem)
 
 """def checkFolder(folder, errFile=sys.stdout):
 	for file in [file for file in os.listdir(folder) if (os.path.isfile(file) and checkFile(file))]:
@@ -22,5 +23,4 @@ def checkFile(filename, errFile=sys.stdout):
 	if filename.endswith(".py"):
 		return True
 	else: 
-		print(filename+" is not a python source code!", file=errFile)
 		return False
