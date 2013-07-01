7fb5256204cb18f2a06283ae2c4a7d7c8dce81cf
13oz <duminsky.nick@gmail.com>
2013-07-01 10:21:20 +0400

    refactored

--- walker.py
+++ walker.py
@@ -3,13 +3,20 @@ import sys
 
 import reader
 
-def walkThrough(folder, errFile=sys.stdout):
+def walkThrough(folder, errFile=sy.stdout):
+	for dir in [dir for dir in os.listdir(folder) if os.path.isdir(dir)]:
+		for file in [file for file in os.listdir(folder) if (os.path.isfile(file) and checkFile(file))]:
+			reader.readFile(file)
+
+"""def checkFolder(folder, errFile=sys.stdout):
+	for file in [file for file in os.listdir(folder) if (os.path.isfile(file) and checkFile(file))]:
+		reader.readFile(file)
 	for file in os.listdir(folder):
-		if os.path.isdir(folder+"/"+file):
-			walkThrough(folder+"/"+file, errFile)
-		if os.path.isfile(folder+"/"+file):
-			if checkFile(folder+"/"+file, errFile):
-				reader.readFile(folder+"/"+file, errFile)
+					if os.path.isdir(folder+"/"+file):
+						walkThrough(folder+"/"+file, errFile)
+					if os.path.isfile(folder+"/"+file):
+						if checkFile(folder+"/"+file, errFile):
+							reader.readFile(folder+"/"+file, errFile)"""
 
 def checkFile(filename, errFile=sys.stdout):
 	if filename.endswith(".py"):
